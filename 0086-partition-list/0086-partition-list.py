80d91e6dd3210086bea55c16dc804fb7f3bc4866
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-01-12 22:25:21 +0100

    Time: 0 ms (100%), Space: 12.6 MB (21.82%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -2,32 +2,23 @@ class Solution(object):
     def partition(self, head, x):
         if not head or not head.next:
             return head
-        
-        # Dummy nodes for "smaller" and "greater/equal" lists
         smaller_head = ListNode(0)
         greater_head = ListNode(0)
         
-        # Pointers to iterate and build the two lists
         smaller = smaller_head
         greater = greater_head
-        cur = head
-        
+        cur = head        
         while cur:
             if cur.val < x:
-                # Add to the "smaller" list
                 smaller.next = cur
                 smaller = smaller.next
             else:
-                # Add to the "greater/equal" list
                 greater.next = cur
                 greater = greater.next
             cur = cur.next
         
-        # Terminate the "greater" list to avoid cycles
         greater.next = None
         
-        # Connect "smaller" list to the "greater/equal" list
         smaller.next = greater_head.next
         
-        # Return the head of the "smaller" list
         return smaller_head.next
