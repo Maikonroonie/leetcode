df77964d5659835b9b3973840f551aa17d6f7587
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-01-31 19:44:36 +0100

    Time: 39 ms (8.44%), Space: 15.2 MB (9.3%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s)!=len(t):
+            return False
+        n=len(s)
+        T1=[]
+        T2=[]
+        for i in range(n):
+            T1.append(s[i])
+            T2.append(t[i])
+        T1.sort()
+        T2.sort()
+        for i in range(n):
+            if T1[i]!=T2[i]:
+                return False
+        return True
+
