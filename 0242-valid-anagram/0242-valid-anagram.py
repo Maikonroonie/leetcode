f4d91257e65b218d07d6ea526ab803f96374b607
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-01-31 19:51:20 +0100

    Time: 19 ms (67.76%), Space: 14.5 MB (9.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,12 +7,19 @@ class Solution(object):
         #T2=[]
        # for i in range(n):
           #  T1.append(s[i])
-         #   T2.append(t[i])
+         #   T2.append(t[i])    #huge brute 
         #T1.sort()
         #T2.sort()
         #for i in range(n):
           #  if T1[i]!=T2[i]:
          #       return False
         #return True
-        return sorted(s) == sorted(t)
+    #    return sorted(s) == sorted(t)    #big brute
+        countS, countT = {} , {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+
 
