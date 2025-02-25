ec7d015a7bf1bf778cc0b72a5a0acb8815e6e390
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-02-25 02:16:08 +0100

    Time: 4 ms (83.23%), Space: 13.7 MB (35.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,10 @@
 class Solution(object):
     def moveZeroes(self, nums):
-        N=len(nums)
-        a=0
-        x=N-1
-        for i in range(N):
-            if nums[a]==0:
-                for j in range(a+1,x+1):
-                    nums[j-1]=nums[j]
-                nums[x]=0
-                x-=1
-            else:
-                a+=1
+        l=0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
         return nums
-                
+
 
