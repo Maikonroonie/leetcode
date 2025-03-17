d05c9a34838b742eb91f0c9fb0db920355e17996
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-03-17 22:41:26 +0100

    Time: 0 ms (100%), Space: 13.3 MB (49.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,25 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        mapa={}
+        mapa={}  #1 O(n) time    O(n) space
         n=len(nums)
         for i in range(n):
             val=target-nums[i]
             if val in mapa:
                 return mapa[val], i
             mapa[nums[i]] = i
+    #def twoSum(self, nums, target):
+     #   nums.sort()# mozna posorotwac samemu heap sortem/ merge sortem/quick sortem/ w nlogn time
+      #  l=0
+       # r=len(nums)-1
+        #while l<r:
+         #   if nums[l]+nums[r]==target:
+          #      return l, r
+           # elif nums[l]+nums[r]<target:  # jeszcze zeby to mialo sens trzeba by zrobic tablice krotek dla zachowania indeksow
+            #        l+=1
+             #   else nums[l]+nums[r]>target:
+              #      r-=1
+            #return 
+            
+
         
         
