f2aafad4818c81f22ddbe6ca7673508d676ea6ce
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-03-18 11:45:11 +0100

    Time: 0 ms (100%), Space: 12.6 MB (11.07%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def sortColors(self, nums):
-        T=[0,0,0]
+        '''T=[0,0,0]
         n=len(nums)
         for i in range(n):
             T[nums[i]]+=1
@@ -16,4 +16,21 @@ class Solution(object):
                 y+=1
             else:
                 nums[i]=2
+        return nums'''
+        #partition
+        def swap(a,b):
+            nums[a], nums[b] = nums[b], nums[a]
+        l=0
+        r=len(nums)-1
+        i=0
+        while i<=r:
+            if nums[i]==0:
+                swap(i, l)
+                l+=1
+            elif nums[i]==2:
+                swap(i, r)
+                i-=1
+                r-=1
+            i+=1
         return nums
+                
