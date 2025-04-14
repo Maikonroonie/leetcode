c76030f2e7e4d5d4fdf694418946d94a1c4ef0ac
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-04-14 14:23:35 +0200

    Time: 39 ms (94.83%), Space: 16.5 MB (81.9%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.py
+++ 0922-possible-bipartition/0922-possible-bipartition.py
@@ -6,13 +6,13 @@ class Solution(object):
             graph[v].append(u)
             graph[u].append(v)
         colors = [None for _ in range(n+1)]
-        for i in range(n):
-            if colors[i] is None:
-                colors[i] = 0
+        for i in range(n):  # this is for unconneted parts of graph (unconnected graph can still be
+            if colors[i] is None:                                                   #bipartitie
+                colors[i] = 0 # we set two colors (0 and 1)
                 q=deque([i])
-                while q:
-                    node=q.pop()
-                    for nb in graph[node]:
+                while q: # and perform deafult dfs in every connected part of the graph and check if we can
+                    node=q.pop()            #color it with two colors, then we return False if we cant,
+                    for nb in graph[node]:              # if we done whole graph, we return True
                         if colors[nb] is None:
                             colors[nb] = 1 - colors[node]
                             q.append(nb)
