94bd20448151a7165a8d6010186c78294fef9f35
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-04-17 13:37:00 +0200

    Time: 211 ms (46.97%), Space: 16.3 MB (36.36%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def scheduleCourse(self, courses):
-        #priority queue
+        #priority queue/ max heap, i use heapq form python but since it is min heap i make -time
         courses.sort(key=lambda x: x[1])
         heap=[]
         max_time = 0
