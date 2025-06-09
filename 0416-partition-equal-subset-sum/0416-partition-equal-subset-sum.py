7f3967782378e5e91abf4ff02c6a5e8d5143a97e
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-06-09 13:28:39 +0200

    Time: 902 ms (54.63%), Space: 12.8 MB (85.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,7 +11,7 @@ class Solution(object):
         dp[0] = True
         
         for num in nums:
-            for i in range(target, num - 1, -1):
+            for i in range(target, num - 1, -1): # tutaj musimy tak isc zeby dwa razy np nie doadac tego sameo numa,  gdybysmy szli na odwrot to ten element pod nami moglby sie zjebac np dp[4] = dp[2] or dp[4], gdzie gdzie dla num =2 dp od dwa dopiero co ustawilismy na true i tu by sie znow ustawilo wiec zle
                 dp[i] = dp[i] or dp[i - num]
         
         return dp[target]
