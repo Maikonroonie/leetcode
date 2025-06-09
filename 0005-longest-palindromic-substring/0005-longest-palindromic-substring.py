f7d9efd177d2d561adbcfa0fa55af46080eda375
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-06-09 09:57:57 +0200

    Time: 3911 ms (21.4%), Space: 21.2 MB (5.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def longestPalindrome(self, s):
+        '''# first idea, here we go from the middle to the left and rigth tracking the longest palindrom
+        # we do it twice first if the len of palindrom is the even number and second time when it is 
+        # and odd number, we return the res that is the longer one
         n=len(s)
         res=""
         res_len=0
@@ -19,5 +22,39 @@ class Solution(object):
                     res_len=r-l+1
                 l-=1
                 r+=1
-        return res
-    
+        return res'''
+
+        #second idea is dynamic programinng approach:
+        # dp[i][j] = True means that substring from index i to j is a palindrom
+        def dynamic(s):
+            n=len(s)
+            if n == 0:
+                return ""
+            elif n==1:
+                return s
+            elif n==2 and s[0]==s[1]:
+                return s
+            elif n==2:
+                return s[0]
+            dp=[[False for _ in range(n)] for _ in range(n)]
+            start=0
+            max_len=1
+            for i in range(n):
+                dp[i][i] = True
+            
+            for i in range(n-1):
+                if s[i] == s[i+1]:
+                    dp[i][i+1] = True
+                    start=i
+                    max_len=2
+            for i in range(3, n+1):
+                for j in range(n-i+1):
+                    k=j+i-1
+                    if s[j] == s[k] and dp[j+1][k-1]:
+                        dp[j][k] = True
+                        if i > max_len:
+                            start = j
+                            max_len = i
+
+            return s[start:start + max_len]
+        return dynamic(s)
