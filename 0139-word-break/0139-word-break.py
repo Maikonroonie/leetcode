b36101586d59ff90c1ec5a5eeea3b15e8b893166
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-06-09 12:41:00 +0200

    Time: 3 ms (87.28%), Space: 12.5 MB (41.69%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -8,3 +8,21 @@ class Solution(object):
                 if len(word) <= i and word == s[i-len(word):i] and dp[i-len(word)] == True:
                     dp[i] = True
         return dp[n]
+
+'''
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        wordSet = set(wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+
+        return dp[n]
+
+''' # druga wersja praktycznie to samo tyle ze zamina tablicy wordict na set przez co zlozonosc pesymistyczna bedzie lepsza O(n *k) n - dlugosc napisu, k - liczba slow w worddict, w pierwszym rozwiazaniu zlozonosc wychodzi O(n^2) (pesymistycznie)
