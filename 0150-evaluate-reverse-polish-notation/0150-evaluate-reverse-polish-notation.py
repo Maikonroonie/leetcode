186588cf8ab77bfa4ed7833d452f54dc94221695
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-07-07 15:08:05 +0200

    Time: 11 ms (37.08%), Space: 18 MB (7.73%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def evalRPN(self, tokens):
+    def evalRPN(self, tokens): # rozwiazanie rekurencyjne
         def operation(a, b, op):
             if op == '+':
                 return a+b
@@ -21,6 +21,27 @@ class Solution(object):
             return int(operation(left, right, token))
             
         return rek()
+
+# rozwizanie za pomocą stacka
+'''
+        class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+'''
 
             
                 
