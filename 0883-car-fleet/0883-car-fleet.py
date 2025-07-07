228a11cea87eef3c643da980348fff9612cd0a27
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-07-07 17:34:57 +0200

    Time: 214 ms (86.57%), Space: 34.5 MB (46.14%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,13 +1,20 @@
 class Solution(object):
     def carFleet(self, target, position, speed):
-        pairs = sorted(zip(position, speed), reverse=True)
-        stack = []
+        stack=[]
+        n=len(position)
+        pos_speed = [ (0, 0) for _ in range(n)]
 
-        for pos, spd in pairs:
-            time = float(target - pos) / spd
-            # jeśli ten samochód dojeżdża później niż ostatni – nowa flota
-            if not stack or time > stack[-1]:
+        for i in range(n):
+            pos_speed[i] = (position[i], speed[i])
+        
+        pos_speed.sort(key = lambda x: x[0], reverse = True)
+
+        # time to target = (target - position)/speed
+        for pos, speed in pos_speed:
+            time = float(target - pos)/speed
+            if not stack or time > stack[-1]: # nowa flota
                 stack.append(time)
-                # inaczej – zlewa się z flotą przed nim
-
+                
         return len(stack)
+
+
