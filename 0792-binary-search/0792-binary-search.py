d4be9a4d427a6a4673e15e9ae53e8fe86b058cbe
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-07-08 12:43:46 +0200

    Time: 0 ms (100%), Space: 13.5 MB (31.89%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -4,7 +4,7 @@ class Solution(object):
         l=0
         r=n-1
         while l<=r:
-            #mid=(l+r)//2
+            #mid=(l+r)//2 in some laungages it may cause overflow
             mid = l + (r-l)//2
             if nums[mid]<target:
                 l=mid+1
