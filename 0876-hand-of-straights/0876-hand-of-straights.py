4d38eab21b73dca86615be23091dfcdd3340996f
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-14 17:13:47 +0200

    Time: 35 ms (54.73%), Space: 19.1 MB (42.27%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,17 +1,36 @@
-
+#nlogn, becouse of sorting
+'''
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        n = len(hand)
-        if n % groupSize != 0:
+        if len(hand) % groupSize:
             return False
-        hand.sort()
-        count = Counter(hand)
 
-        for card in hand:
-            if count[card] == 0:
-                continue
-            for x in range(card, card + groupSize):
-                if count[x] == 0:
-                    return False
-                count[x] -= 1
+        count = Counter(hand) # to jest slownik mowiacy ile razy cos wystepuje
+        hand.sort()
+        for num in hand:
+            if count[num]:
+                for i in range(num, num + groupSize):
+                    if not count[i]:
+                        return False
+                    count[i] -= 1
+        return True
+        '''
+#O(n)
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        count = Counter(hand)
+        for num in hand:
+            start = num
+            while count[start - 1]: # here we look for the start of sequence
+                start -= 1
+            while start <= num:
+                while count[start]: # here we just go like in the nlogn approach
+                    for i in range(start, start + groupSize):
+                        if not count[i]:
+                            return False
+                        count[i] -= 1
+                start += 1
         return True
