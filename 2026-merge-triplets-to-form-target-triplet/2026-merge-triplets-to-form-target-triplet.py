0ec992c9adcad33efcfc1e8f4fe704fab890741b
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-14 18:30:33 +0200

    Time: 11 ms (78.37%), Space: 55.1 MB (57.05%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        a, b, c = target
+        found0 = found1 = found2 = False
+        
+        for x, y, z in triplets:
+            if x <= a and y <= b and z <= c:
+                if x == a: found0 = True
+                if y == b: found1 = True
+                if z == c: found2 = True
+        
+        return found0 and found1 and found2
