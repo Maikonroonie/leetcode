0c148541de4e9958120ecf7ba177b0a903dea3a3
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-14 19:00:07 +0200

    Time: 4 ms (63.81%), Space: 17.7 MB (84.06%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,3 +1,4 @@
+#moje rozwiazanie lekko przekombinowane ale w zasadzie z grubsza to samo co we flagowym rozwiazaniu
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         res=[]
@@ -25,4 +26,21 @@ class Solution:
                 cur_cnt=0
 
         return res
+#flagowe rozwiazanie
+'''
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastIndex = {}
+        for i, c in enumerate(s):
+            lastIndex[c] = i
 
+        res = []
+        size = end = 0
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+
+            if i == end:
+                res.append(size)
+                size = 0
+        return res'''
