6b204557417101544b64dbb3fb2777ab3545cdbc
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-15 17:00:32 +0200

    Time: 0 ms (100%), Space: 17.9 MB (38.66%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,3 +1,4 @@
+'''
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n <= 0:
@@ -5,3 +6,16 @@ class Solution:
         while n % 4 == 0:
             n //= 4
         return n == 1
+'''
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0 or (n & (n - 1)) != 0:  # nie potęga 2 → false
+            return False
+        # przesuwamy bity parami
+        while n > 1:
+            n >>= 2
+        return n == 1
+
+# n & (n-1) == 0  - oznacza ze to jest potega dwojki bo taka liczba ma tylko jeden bit na 1 reszte na zero
+# Potęgi 4 mają ustawiony bit co 2 pozycje, więc każde przesunięcie o 2 pozycje w prawo usuwa dokładnie dwa zera.
+#W końcu zostanie 1, jeśli to potęga 4.
