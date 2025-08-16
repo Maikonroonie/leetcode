ad550a259d1aa34632fcd29c1b42a46d28ff02eb
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-16 18:28:07 +0200

    Time: 271 ms (73.73%), Space: 38.7 MB (62.48%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,31 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        # the ,key is to track the minimum element from each list and sliding window to track the
+        # range of numbers
+        
+        # Iterate through the lists by always popping the minimum element from the heap (by heap's nature) 
+        # and checking the range between the minimum and the current maximum.
+        n=len(nums) # the amount of lists
+        heap = []
+        cur_max = -float('inf')
+        for i in range(n):
+            heapq.heappush(heap, (nums[i][0], i, 0))
+            cur_max = max(cur_max, nums[i][0])
+        small = [-float('inf'), float('inf')] # the range of current smalles slindng window
+        while heap:
+            cur_min, list_idx, i = heapq.heappop(heap)
+            
+            if cur_max - cur_min < small[1] - small[0]:
+                small = [cur_min, cur_max] # updateing the range
+
+            if i+1 < len(nums[list_idx]):
+                nxt = nums[list_idx][i+1]
+                heapq.heappush(heap, (nxt, list_idx, i+1))
+                cur_max = max(cur_max, nxt)
+            else:
+                break
+        return small
+
+
+
+
