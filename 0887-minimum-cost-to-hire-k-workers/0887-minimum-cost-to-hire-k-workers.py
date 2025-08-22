493a8cbadf90d0422e12b9ebbc46790740ff68b8
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-22 18:19:54 +0200

    Time: 44 ms (46.11%), Space: 20.2 MB (26.58%) - LeetHub

--- 0887-minimum-cost-to-hire-k-workers/0887-minimum-cost-to-hire-k-workers.py
+++ 0887-minimum-cost-to-hire-k-workers/0887-minimum-cost-to-hire-k-workers.py
@@ -7,6 +7,12 @@ class Solution:
             A.append((wage[i]/quality[i], quality[i])) # placa / wydajnosc - najoptymalniejsi pracownicy
         A.sort()
         
+        # every worker at the end will get same money ratio (wage[i]/wage[j] = quality[i]/quality[j])
+        # so at first we add first k workers with the bes efiicinecy ( ratio ), and then we follow the min
+        # res while adding next workers we increase ratio but we reduce money, becouse we keep in heap max
+        # qualities what going further we remove from the (potential hiring staff) at the end we have
+        # the minimum value which is sum(quality) * (ratio(wage / quality) - currently the biggest)
+
         heap=[]
         money = 0
         res = inf
