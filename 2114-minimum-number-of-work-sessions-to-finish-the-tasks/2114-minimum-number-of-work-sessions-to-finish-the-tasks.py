bee8b33a399d591bbc2a8e1f14feb185101c11e8
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-24 20:35:53 +0200

    Time: 4916 ms (24.3%), Space: 17.7 MB (97.61%) - LeetHub

--- 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.py
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/2114-minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -18,8 +18,11 @@ class Solution:
             sub = mask
             while sub:
                 if total[sub] <= sessionTime:
-                    dp[mask] = min(dp[mask], dp[mask ^ sub] +1)
+                    dp[mask] = min(dp[mask], dp[mask ^ sub] +1) # zadania w mask ktore nie nalezą do sub
                 sub = (sub -1) & mask # przechodzimy po wszystkich podmaskach
+# why (sub -1) & mask sub - 1 zmienia najmniej znaczyacy bit 1 na 0, a reszte po prawej ustawia na 1
+# ale po tym moga powstac bity ktore nie naleza do mask wiec robimy & mask
+    
 
         return dp[(1<<n) -1]
 
