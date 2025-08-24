c670e42e9a8cfdd8d9d72ed45b8bf6aa0c401e13
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-24 14:58:01 +0200

    Time: 1653 ms (78.99%), Space: 117.3 MB (90.29%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.py
+++ 0464-can-i-win/0464-can-i-win.py
@@ -1,3 +1,4 @@
+#top - bottom
 class Solution:
     def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
         # maską reprezentujemy liczby ktore zostaly zużyte
@@ -24,4 +25,39 @@ class Solution:
             return False
             
         return canwin(0, desiredTotal)
+'''
+#bottom - up
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:
+        if (maxChoosableInteger * (maxChoosableInteger + 1)) // 2 < desiredTotal:
+            return False
+        if desiredTotal <= maxChoosableInteger:
+            return True
 
+        n = maxChoosableInteger
+        total_states = 1 << n #ilosc wzsystkich masek chyba to bedzie 2^n
+        DP = [False] * total_states
+        sums = [0] * total_states   # sums[mask] = suma wybranych liczb w masce
+
+        # precompute sums
+        for mask in range(total_states):
+            s = 0
+            for i in range(n):
+                if (mask >> i) & 1:
+                    s += i+1
+            sums[mask] = s
+
+        # przechodzimy po wszystkich stanach od pełnych masek do pustej
+        for mask in range(total_states-1, -1, -1):
+            for i in range(n):
+                if not (mask >> i) & 1:  # liczba (i+1) jest wolna, czyli na pozyci i od prawej
+                    if sums[mask] + (i+1) >= desiredTotal:
+                        DP[mask] = True
+                        break
+                    if not DP[mask | (1<<i)]: #stan po tym jak wybiore i+1
+                        DP[mask] = True # czyli jesli w kolejnym stanie nie da sie wygrac
+                        break # to w tym wygralismy i essa (wybierajac liczbe i+1)
+
+        return DP[0] # zwracamy dp[0] - bo to stan poczatkowy zadna liczba nie zostala uzyta
+
+'''
