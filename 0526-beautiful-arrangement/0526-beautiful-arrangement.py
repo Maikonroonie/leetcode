0fb833a19f25d30337f0cce1b600e25391c5f090
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-24 18:35:59 +0200

    Time: 39 ms (97.86%), Space: 19.1 MB (13.5%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -19,7 +19,6 @@ class Solution:
             for j in range(1, n+1):
                 if (i % j == 0 or j % i == 0) and not ((mask >> j) & 1):
                     res += dp(i+1, mask | 1 << j)
-                  #  memo[mask] = res
+            memo[mask] = res
             return res
-           # memo[mask] = res
         return dp(1, 0)
