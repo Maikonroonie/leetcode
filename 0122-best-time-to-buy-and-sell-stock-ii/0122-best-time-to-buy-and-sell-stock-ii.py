17e9b29e935c4a34cfccce6be26f3716eb6513eb
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-25 15:13:54 +0200

    Time: 0 ms (100%), Space: 19 MB (29.87%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
+
+
+'''
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         heap = []
         buy = prices[0]
@@ -23,13 +33,7 @@ class Solution:
             profit += -heapq.heappop(heap) - buy
 
         return profit
+'''
 
-'''
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                profit += prices[i] - prices[i-1]
-        return profit
-'''
+
+
