c640682c480e1579f7067d2d57370e90f3f55eea
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-08-29 13:14:38 +0200

    Time: 153 ms (66.5%), Space: 29.3 MB (90.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,42 +1,14 @@
-class Solution(object):
-    def largestRectangleArea(self, heights):
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         n=len(heights)
-        next_smaller=[-1 for _ in range(n)]
-        prev_smaller=[-1 for _ in range(n)]
-        stack1=[]
-        stack2=[]
-        # next smaller
-        for i in range(n-1, -1, -1):
-            while stack1 and heights[stack1[-1]] >= heights[i]:
-                stack1.pop()
-            if stack1:
-                next_smaller[i] = stack1[-1]
-            stack1.append(i)
-        
-        #prev smaller
-        for i in range(n):
-            while stack2 and heights[stack2[-1]] >= heights[i]:
-                stack2.pop()
-            if stack2:
-                prev_smaller[i] = stack2[-1]
-            stack2.append(i)
-        
-        # teraz area dla i = heihgts[i] * (next_smaller - prev_smaller)
+        stack = []
         max_area = 0
-        cur_area = 0
-        for i in range(n):
-            if next_smaller[i] != -1 and prev_smaller[i] != -1:
-                cur_area = heights[i] * (next_smaller[i] - prev_smaller[i] -1)
-                max_area = max(cur_area, max_area)
-            if prev_smaller[i] == -1 and next_smaller[i]!= -1:
-                cur_area = heights[i] * ( next_smaller[i] )
-                max_area = max(cur_area, max_area)
-            if prev_smaller[i] != -1 and next_smaller[i] == -1:
-                cur_area = heights[i] * ( n - prev_smaller[i]-1)
-                max_area = max(cur_area, max_area)
-            if prev_smaller[i] == -1 and next_smaller[i] == -1:
-                cur_area = heights[i] * (n)
-                max_area = max(cur_area, max_area)
-           # print((cur_area, next_smaller[i], prev_smaller[i]))        
-        return max_area
         
+        for i in range(n+1):
+            h = heights[i] if i < n else 0
+            while stack and heights[stack[-1]] > h:
+                hg = heights[stack.pop()] # tablica przechowuje indeksy
+                wg = i if not stack else i - stack[-1] -1
+                max_area = max(max_area, hg*wg)
+            stack.append(i)
+        return max_area
