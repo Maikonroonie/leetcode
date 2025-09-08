6d6e1279b57df0f0dd611d7958119c3e0ec78cd4
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-09-08 14:58:09 +0200

    Time: 0 ms (100%), Space: 17.8 MB (53.83%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -9,12 +9,10 @@ class Solution:
             if num == prev + 1:
                 prev = num
             else:
-                # domknij dotychczasowy przedział
                 if start == prev:
                     res.append(f"{start}")
                 else:
                     res.append(f"{start}->{prev}")
-                # zacznij nowy przedział od bieżącej liczby
                 start = prev = num
 
         # domknij ostatni przedział
