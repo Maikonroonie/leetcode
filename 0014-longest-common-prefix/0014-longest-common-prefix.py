135b8070fb18114da55f74285896ddfddd94fc9e
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-09-08 13:53:50 +0200

    Time: 0 ms (100%), Space: 17.7 MB (78.68%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,5 +1,42 @@
+
+class TrieNode:
+    __slots__ = ("children", "end")
+    def __init__(self):
+        self.children = {}       # char -> TrieNode
+        self.end = False         # czy kończy się tu słowo
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        # 1) Zbuduj trie
+        root = TrieNode()
+        for s in strs:
+            node = root
+            for ch in s:
+                if ch not in node.children:
+                    node.children[ch] = TrieNode()
+                node = node.children[ch]
+            node.end = True
+
+        # 2) Zejdź jedyną ścieżką dopóki jest jedno dziecko i nie trafimy na koniec słowa
+        res = []
+        node = root
+        while True:
+            if node.end:                # jakiś string kończy się tutaj -> dalej nie ma wspólnego prefiksu
+                break
+            if len(node.children) != 1: # rozgałęzienie lub brak dzieci -> stop
+                break
+            (ch, nxt), = node.children.items()  # jedyne dziecko
+            res.append(ch)
+            node = nxt
+
+        return "".join(res)
+
+
+#class Solution:
+  #  def longestCommonPrefix(self, strs: List[str]) -> str:
         # brute force approach
         '''
         res = ''
@@ -15,7 +52,7 @@ class Solution:
                     return res
             res += st
         return res
-        '''
+        # poruwnywanie najmniejszego i najwiekszego stringa rozwiazauje ten problem duzo optymalniej
         if not strs:
             return ""
         a = min(strs)
@@ -24,4 +61,9 @@ class Solution:
         while i < len(a) and i < len(b) and a[i] == b[i]:
             i += 1
         return a[:i]
+        '''
+        # najlepsze rozwiazanie jest dzieki triee, drzewa prefiksowego 
+
+
+
 
