48a91f142ee3cc763d70d622f2a921cfae4a8bd6
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-10-08 12:33:12 +0200

    Time: 71 ms (81.85%), Space: 33.7 MB (57.12%) - LeetHub

--- 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.py
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.py
@@ -15,4 +15,24 @@ class Solution:
             else:
                 j += 1
         return res
-        
+
+# ta wersja z bisectem jest mlogn ale mogla by byc lepsza gdyby np bylo malo zawodnikow a bardzo duzo trenerow czli male n i duze m to to mogloby byc lepsze
+        '''
+        import bisect
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        res = 0
+        lo = 0
+        n = len(trainers)
+        for p in players:
+            idx = bisect.bisect_left(trainers, p, lo, n)
+            if idx == n:
+                break
+            res += 1
+            lo = idx + 1
+        return res
+
+        '''
