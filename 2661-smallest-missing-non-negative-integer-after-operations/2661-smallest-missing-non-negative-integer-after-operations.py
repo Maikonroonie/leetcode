0e9f673d3e66b5f3750045c34267e24b5a49c33c
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-10-16 17:10:49 +0200

    Time: 79 ms (80.43%), Space: 31.5 MB (92.8%) - LeetHub

--- 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
+++ 2661-smallest-missing-non-negative-integer-after-operations/2661-smallest-missing-non-negative-integer-after-operations.py
@@ -1,11 +1,16 @@
 class Solution:
     def findSmallestInteger(self, nums: List[int], value: int) -> int:
-        # counts of residues 0..value-1
+        # tablia reszt ile mamy reszt danego rodzaju
         cnt = [0] * value
         for a in nums:
-            r = a % value  # Python daje resztę 0..value-1 nawet dla ujemnych
+            r = a % value
             cnt[r] += 1
-
+        # teraz idziemy po kazdej kolejnej liczbie od zera i 
+        #sprawdzamy jej reszte przy dzeieleniu przez value
+        # i teraz sprawdzamy czy w cnt mamy jeszcz dostepne dla tej reszty
+        # jesli tak to cnt[r] zmiejszamy o jeden i idziemy dalej (x += 1)
+        # jak cnt[r] == 0 to znaczy ze musimy juz zwrocic i to jest pierwsza liczba
+        # ktorej nie osiagniemy (MEX)
         x = 0
         while True:
             r = x % value
