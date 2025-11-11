61d2c630200737601f1be588d0dc1286f92f5b26
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-11 22:37:42 +0100

    Time: 3 ms (8.2%), Space: 18.7 MB (73.09%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,17 +1,12 @@
-class Solution(object):
-    def search(self, nums, target):
-        n=len(nums)
-        l=0
-        r=n-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) -1
         while l<=r:
-            #mid=(l+r)//2 in some laungages it may cause overflow
-            mid = l + (r-l)//2
-            if nums[mid]<target:
-                l=mid+1
-            elif nums[mid] > target:
-                r=mid-1
+            mid = (l+r)//2
+            if target > nums[mid]:
+                l = mid + 1
+            elif target < nums[mid]:
+                r = mid - 1
             else:
                 return mid
         return -1
-
-
