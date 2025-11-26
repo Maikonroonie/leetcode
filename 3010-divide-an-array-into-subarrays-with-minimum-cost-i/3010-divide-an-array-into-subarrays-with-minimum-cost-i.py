f185069cc31d26b2b7dae56448518d09cff8f182
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-26 23:47:45 +0100

    Time: 3 ms (38.52%), Space: 17.6 MB (78.8%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.py
@@ -3,8 +3,7 @@ class Solution:
         n = len(nums)
         # minimum suma
         res = nums[0]
-        A = nums[1:n]
-
+        A = nums[1:]
         A.sort()
         return res + A[0] + A[1]
 
