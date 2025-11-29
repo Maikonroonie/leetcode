b77634f4c057fc7df714ba1b1918a3af175997aa
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-29 18:57:05 +0100

    Time: 364 ms (44.69%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,44 @@
 # Write your MySQL query statement below
+/*
 select email as Email
 from Person
 group by email
 having count(email) > 1;
+*/
+SELECT DISTINCT email
+FROM (
+    SELECT 
+        email, 
+        COUNT(*) OVER (PARTITION BY email) as cnt
+    FROM Person
+) as t
+WHERE cnt > 1;
+
+/* self join
+SELECT DISTINCT p1.email
+FROM Person p1
+JOIN Person p2 
+    ON p1.email = p2.email     
+    AND p1.id != p2.id;
+*/
+
+/* podzapytanie
+SELECT DISTINCT p1.email
+FROM Person p1
+WHERE (
+    SELECT COUNT(*) 
+    FROM Person p2 
+    WHERE p1.email = p2.email
+) > 1;
+*/
+
+/* cross apply
+SELECT DISTINCT p1.email
+FROM Person p1
+CROSS APPLY (
+    SELECT COUNT(*) as cnt
+    FROM Person p2
+    WHERE p1.email = p2.email
+) as c
+WHERE c.cnt > 1;
+*/
