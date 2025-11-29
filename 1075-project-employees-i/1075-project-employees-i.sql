bde6bd6da890219f02ca1b8662528e7c175e2725
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-29 22:18:06 +0100

    Time: 1155 ms (59.13%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+select p.project_id, 
+round(avg(cast(e.experience_years as decimal(10, 2))), 2) as average_years
+from Project p join Employee e
+on p.employee_id = e.employee_id
+group by p.project_id
+order by p.project_id
