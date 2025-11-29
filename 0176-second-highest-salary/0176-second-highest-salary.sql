8df525bc6aad553cefd3f774c14670db3db2c012
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-29 22:38:21 +0100

    Time: 402 ms (32.81%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,12 +1,12 @@
 /* Write your T-SQL query statement below */
 /*
-select top 1 salary as SecondHighestSalary
-from(
-    select top 2 salary
-    from Employee
-    order by salary desc
-    ) as topsalaries
-order by salary 
+SELECT (
+    SELECT DISTINCT salary    -- DISTINCT, żeby usunąć duplikaty (np. dwa razy 200)
+    FROM Employee
+    ORDER BY salary DESC
+    OFFSET 1 ROWS             -- Pomiń 1 (najwyższy)
+    FETCH NEXT 1 ROW ONLY     -- Weź następny
+    ) AS SecondHighestSalary;
 */
 select max(e1.salary) as SecondHighestSalary
 from Employee e1
@@ -14,4 +14,5 @@ where e1.salary < (select
     max(e2.salary)
     from employee e2
 ) 
+
 
