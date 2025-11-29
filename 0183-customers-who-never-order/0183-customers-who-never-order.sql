44a4c7379041fdbbd51b2895317bc4fcdd216691
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-29 19:17:33 +0100

    Time: 481 ms (86.74%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -3,3 +3,13 @@ select c.name as Customers
     from Customers c
     left join Orders o on c.id = o.customerId
         where o.customerId is Null;
+
+# opcja 2 podzapytnie
+/*
+select name as Customers
+from Customers
+where id not in (
+    select customerId
+    from Orders
+)
+*/
