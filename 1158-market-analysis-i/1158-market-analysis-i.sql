d5b6366f0e066c03f84ca9116db8294984e0c2cf
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-11-30 01:38:08 +0100

    Time: 1286 ms (60.16%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,22 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    u.user_id AS buyer_id,
+    u.join_date,
+    SUM(CASE WHEN YEAR(o.order_date) = 2019 THEN 1 ELSE 0 END) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o ON u.user_id = o.buyer_id
+GROUP BY u.user_id, u.join_date;
+
+/*
+SELECT 
+    u.user_id AS buyer_id,
+    u.join_date,
+    ISNULL(o.cnt, 0) AS orders_in_2019  -- Zamieniamy NULL na 0
+FROM Users u
+LEFT JOIN (
+    SELECT buyer_id, COUNT(order_id) as cnt
+    FROM Orders
+    WHERE YEAR(order_date) = 2019       -- Filtrujemy tylko rok 2019
+    GROUP BY buyer_id                   -- Grupujemy po KLIENCIE, nie dacie
+) o ON u.user_id = o.buyer_id;
+*/
