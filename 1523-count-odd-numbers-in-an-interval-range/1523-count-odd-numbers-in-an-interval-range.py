e2c9af96b4f3c09ae11c2f880d6c235570d1bae7
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-12-07 12:00:52 +0100

    Time: 37 ms (56.81%), Space: 17.7 MB (74.03%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -2,9 +2,7 @@ class Solution:
     def countOdds(self, low: int, high: int) -> int:
         # chcemy zwrocic liczbe liczb nieparzystych z przedzialu <low, high>
         diff = high - low 
-        if low % 2 != 0:
+        if low % 2 != 0 or (low % 2 == 0 and high % 2 !=0):
             return diff//2 + 1
-        elif low % 2 == 0 and high % 2 !=0:
-            return diff//2 +1
         else:
             return diff//2
