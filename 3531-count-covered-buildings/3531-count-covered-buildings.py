b1902a71fd4b1328015a6ad03eca0a00ba7ea3f8
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-12-11 16:07:26 +0100

    Time: 425 ms (85.71%), Space: 56.6 MB (82.35%) - LeetHub

--- 3531-count-covered-buildings/3531-count-covered-buildings.py
+++ 3531-count-covered-buildings/3531-count-covered-buildings.py
@@ -12,9 +12,9 @@ class Solution:
             col_min[x] = min(col_min[x], y)
 
         res = 0
-        print((row_max, row_min, col_max, col_min))
+        #print((row_max, row_min, col_max, col_min))
         for x, y in buildings:
             if row_min[y] < x < row_max[y] and col_min[x] < y < col_max[x]:
                 res += 1
-                print((x, y))
+                #print((x, y))
         return res
