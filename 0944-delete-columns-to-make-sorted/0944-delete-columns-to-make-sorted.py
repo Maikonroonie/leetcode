e2d18220ce3936def437b2df0bdce1fe87be0bab
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-12-20 13:50:51 +0100

    Time: 74 ms (20.98%), Space: 18.5 MB (37.07%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        n = len(strs[0])
+        res = 0
+        is_g = [True for _ in range(n)]
+        last_letter = [None for _ in range(n)]
+        
+        for i in range(n):
+            for idx, s in enumerate(strs):
+                letter = s[i]
+                if not is_g[i]:
+                    continue
+                elif last_letter[i] is None:
+                    last_letter[i] = letter
+                elif last_letter[i] <= letter:
+                    last_letter[i] = letter
+                else:
+                    print(last_letter[i])
+                    print(letter)
+                    is_g[i] = False
+                    res += 1
+        
+        return res
