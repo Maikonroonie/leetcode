aa10ab5d1788be138d50dc30bd507af782d21d80
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2025-12-23 16:23:03 +0100

    Time: 141 ms (93.31%), Space: 56.1 MB (96.46%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,17 +1,20 @@
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
+        start_sorted = sorted(events)
+        end_sorted = sorted(events, key=lambda x: x[1])
+        end_sorted = deque(end_sorted)
 
-        end_sorted = deque(sorted(events, key=itemgetter(1)))
-        start_sorted = sorted(events, key=itemgetter(0))
+        res = max(v for s, e, v in events)
+        end_max = 0
 
-        ans = max(v for _, _, v in events)
+        for s, e, v in start_sorted:
+            while end_sorted and end_sorted[0][1] < s:
+                _, _, val = end_sorted.popleft()
+                end_max = max(end_max, val)
+            res = max(res, v + end_max)
+        
+        return res
+            
 
-        end_max = 0  
 
-        for start, end, value in start_sorted:
-            while end_sorted and end_sorted[0][1] < start:
-                _, _, v = end_sorted.popleft()
-                end_max = max(end_max, v)
-            ans = max(ans, value + end_max)
-
-        return ans
+        
