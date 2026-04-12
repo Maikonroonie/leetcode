debe1186cfff0e85b1e16ad12a4a2e566399351c
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-04-12 18:57:52 +0200

    Time: 183 ms (69.5%), Space: 35.4 MB (35.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        '''
         res= []
         heap = [] #maxheap
         for i, num in enumerate(nums):
@@ -14,6 +15,25 @@ class Solution:
                 if idx > i - k +1:
                     heapq.heappush(heap, (val, idx))
         return res
+        '''
+        # o(n) try
+        res = []
+        dq = deque() 
+        
+        for i, num in enumerate(nums):
+            while dq and nums[dq[-1]] < num:
+                dq.pop()
+            dq.append(i)
+            
+            if dq[0] < i - k + 1:
+                dq.popleft()
+            
+            if i >= k - 1:
+                res.append(nums[dq[0]])
+                
+        return res
+
+
 
 
                 
