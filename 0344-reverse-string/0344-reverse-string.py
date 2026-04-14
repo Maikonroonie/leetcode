9436a6d9b2f173083e5ec9e13f29ac179ff8300a
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-04-14 15:25:04 +0200

    Time: 3 ms (48.06%), Space: 23.3 MB (85.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,6 +3,15 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+        '''
         s.reverse()
         return s
-        
+        '''
+        # let try to do it on my own xd
+        n = len(s)
+        l = 0
+        r = n-1
+        while l<r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
