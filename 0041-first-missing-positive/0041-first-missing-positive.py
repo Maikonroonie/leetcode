2518ce52b28da1db14e73f5021baceb87efa3d8f
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-04-14 14:40:43 +0200

    Time: 79 ms (5.14%), Space: 30.8 MB (86.09%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,5 +1,6 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        '''
         n = len(nums)
         i = 0
         while i < n:
@@ -18,3 +19,21 @@ class Solution:
                 return i + 1
 
         return n + 1
+        '''
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
+                if nums[val - 1] > 0:
+                    nums[val - 1] *= -1
+                elif nums[val - 1] == 0:
+                    nums[val - 1] = -1 * (len(nums) + 1)
+
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+
+        return len(nums) + 1
