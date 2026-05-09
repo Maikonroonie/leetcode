2c4512ae24eab2cbb2a606c3e4bd84de8984147a
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-05-09 20:46:34 +0200

    Time: 35 ms (49.1%), Space: 32.5 MB (67.71%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        '''
         d = defaultdict(list)
         for i, num in enumerate(nums):
             d[num].append(i)
@@ -10,6 +11,19 @@ class Solution:
             for i in range(n-1):
                 if abs(idx_list[i] - idx_list[i+1]) <= k:
                     return True
+        return False
+        '''
+        window_set = set()
+        
+        for i in range(len(nums)):
+            if i > k:
+                window_set.remove(nums[i - k - 1])
+            
+            if nums[i] in window_set:
+                return True
+            
+            window_set.add(nums[i])
+            
         return False
             
 
