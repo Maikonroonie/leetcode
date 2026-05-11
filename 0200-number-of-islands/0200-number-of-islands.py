76566fb9c7e63c1117f8b30dc328493a62a155a9
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-05-11 22:56:13 +0200

    Time: 256 ms (34.4%), Space: 26.8 MB (21.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,31 @@
-class Solution(object):
-    def numIslands(self, grid):
-        visited=set()
-        n=len(grid)
-        m=len(grid[0])
-        cntr=0
-        def search(i, j):
-            if i<0 or i>=n or j<0 or j>=m:
-                return
-            if grid[i][j]!='1' or (i, j) in visited:
-                return
-            visited.add((i, j))
-            search(i+1, j)
-            search(i-1, j)
-            search(i, j+1)
-            search(i, j-1)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        res = 0
+        q = deque()
+        visited = set()
+        n = len(grid)
+        m = len(grid[0])
 
+        def bfs(i, j):
+            q = deque()
+            visited.add((i, j))
+            q.append((i, j))
+
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < n and 0 <= nc < m and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+                            
         for i in range(n):
             for j in range(m):
-                if (i, j) not in visited and grid[i][j]=='1':
-                    cntr+=1
-                    search(i, j)
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    res += 1
+                    bfs(i, j)
 
-
-        return cntr
+        return res
+                    
+        
