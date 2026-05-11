6bfc164d80545b6febb3de5d105c7b34a1f4e472
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-05-11 23:21:00 +0200

    Time: 3 ms (87.36%), Space: 20.5 MB (64.63%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,24 @@
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        graph=[[] for _ in range(numCourses)]
-        for u, v in prerequisites:
-            graph[u].append(v)
-        # graf nie moze miec cyklow chyba if cykl return false else return true
-        visited = [0] * numCourses
-        def dfs(s):
-            if visited[s]==1:
-                return False
-            if visited[s]==2:
-                return True
-            visited[s]=1
-            for nb in graph[s]:
-                if dfs(nb) == False:
-                    return False
-            visited[s]=2
-            return True
-        for i in range(numCourses):
-            if dfs(i) == False:
-                return False
-        return True
+from collections import deque
+# topological sort (khan)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        
+        for course, pre in prerequisites:
+            adj[pre].append(course)
+            indegree[course] += 1
+
+        q = deque([i for i in range(numCourses) if indegree[i] == 0])
+
+        count = 0
+        while q:
+            node = q.popleft()
+            count += 1
+            
+            for next_node in adj[node]:
+                indegree[next_node] -= 1
+                if indegree[next_node] == 0:
+                    q.append(next_node)
+
+        return count == numCourses
