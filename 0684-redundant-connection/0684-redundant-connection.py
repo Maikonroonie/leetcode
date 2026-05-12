5c1add36e3c62c1c5e9997ad6d8ba8f5315bd850
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-05-12 13:00:15 +0200

    Time: 3 ms (60.52%), Space: 19.7 MB (51.66%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,27 +1,32 @@
 
 class Solution:
     def findRedundantConnection(self, edges):
-        n=len(edges)
-        parent=[i for i in range(n+1)]
+        n = max(max(u, v) for u, v in edges) + 1
+        parent = [i for i in range(n+1)]
         rank = [0 for _ in range(n+1)]
         def find(u, parent):
-            if parent[u]!=u:
-                parent[u]=find(parent[u], parent)
+            if parent[u] != u:
+                parent[u] = find(parent[u], parent)
             return parent[u]
+        
         def union(u, v, parent, rank):
             root_u = find(u, parent)
             root_v = find(v, parent)
 
             if root_u != root_v:
-                if rank[root_u] > rank[root_v]:
+                if rank[root_u] < rank[root_v]:
                     parent[root_v] = root_u
-                elif rank[root_u] < rank[root_v]:
+                elif rank[root_u] > rank[root_v]:
                     parent[root_u] = root_v
                 else:
                     parent[root_v] = root_u
-                    rank[root_u] += 1
-                return True  # Union succeeded
-            return False  # u and v were already connected (cycle)
+                    rank[root_u] +=1
+                return True
+            return False
+        
         for u, v in edges:
             if not union(u, v, parent, rank):
                 return [u, v]
+
+
+
