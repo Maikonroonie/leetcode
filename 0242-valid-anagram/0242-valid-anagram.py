759a5f3c72cfe05df3f61c606bca1ee956df738a
Mikołaj Gaweł <mikolaj.gawel@gmail.com>
2026-07-27 12:08:35 +0200

    Time: 13 ms (51.7%), Space: 19.2 MB (99.27%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,25 +1,12 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s)!=len(t):
-            return False
-        #n=len(s)
-        #T1=[]
-        #T2=[]
-       # for i in range(n):
-          #  T1.append(s[i])
-         #   T2.append(t[i])    #huge brute 
-        #T1.sort()
-        #T2.sort()
-        #for i in range(n):
-          #  if T1[i]!=T2[i]:
-         #       return False
-        #return True
-    #    return sorted(s) == sorted(t)    #big brute
-        countS, countT = {} , {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
-
-
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d = defaultdict(int)
+        for ch in s:
+            d[ch] += 1
+        for ch in t:
+            d[ch] -= 1
+        
+        for key, val in d.items():
+            if val != 0:
+                return False
+        return True
